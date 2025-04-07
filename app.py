814b535a617d70a8ab00543ec13bad1660d4704e
Sriram2226 <sriram2005z@gmail.com>
2025-04-07 18:10:01 +0530

    cors issue fix #1

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://sentimentreviewer.onrender.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
