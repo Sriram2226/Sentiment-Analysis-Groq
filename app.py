c373ec90251be9afcb98441909533ab0d2a3b1e6
SRIRAM P <136807365+Sriram2226@users.noreply.github.com>
2025-04-12 18:18:56 +0000

    cors check

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://sentimentreviewer.onrender.com"],
+    # allow_origins=["https://sentimentreviewer.onrender.com"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
